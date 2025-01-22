ca71d5929b3faf4c5bf636e2e2183306c9b581f1
Daniel Borisenko <danikborisen19@gmail.com>
2025-01-22 12:02:37 -0500

    created website

---
+++ WebsiteApp/WebsiteApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebsiteApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
