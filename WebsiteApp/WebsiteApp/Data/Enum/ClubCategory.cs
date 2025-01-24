b3386a28c30b1960f97ac26f10fb2d0c170bc450
Daniel Borisenko <danikborisen19@gmail.com>
2025-01-24 16:43:58 -0500

    Created Models and Enums.

---
+++ WebsiteApp/WebsiteApp/Data/Enum/ClubCategory.cs
@@ -0,0 +1,11 @@
+﻿namespace WebsiteApp.Data.Enum
+{
+    public enum ClubCategory
+    {
+        RoadRunner,
+        Womens,
+        City,
+        Trail,
+        Endurance
+    }
+}
---
+++ WebsiteApp/WebsiteApp/Data/Enum/RaceCategory.cs
@@ -0,0 +1,11 @@
+﻿namespace WebsiteApp.Data.Enum
+{
+    public enum RaceCategory
+    {
+        Marathon,
+        Ultra,
+        Fivek,
+        Tenk,
+        HalfMarathon
+    }
+}
