a470e04595a01cf6600bb6ac805eef038804cb7f
Daniel Borisenko <danikborisen19@gmail.com>
2025-01-29 18:38:59 -0500

    Added classes; added sql

---
+++ WebsiteApp/WebsiteApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebsiteApp.Models;
+
+namespace WebsiteApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Race> Races { get; set; }
+        public DbSet<Club> CLubs { get; set; } 
+        public DbSet<Address> Addresses { get; set; }
+    }
+}
