d29a6b76ee546fc9c8195d0764e0f3d7b96b3e88
Daniel Borisenko <danikborisen19@gmail.com>
2025-02-04 18:45:36 -0500

    Made controllers

---
+++ WebsiteApp/WebsiteApp/Controllers/ClubController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebsiteApp.Controllers
+{
+    public class ClubController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
