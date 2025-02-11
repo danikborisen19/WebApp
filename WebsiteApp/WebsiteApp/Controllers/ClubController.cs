95e403e1bb252e733a40b88409fc9c19633f6853
Daniel Borisenko <danikborisen19@gmail.com>
2025-02-11 16:07:07 -0500

    not sure, i forgot

--- WebsiteApp/WebsiteApp/Controllers/ClubController.cs
+++ WebsiteApp/WebsiteApp/Controllers/ClubController.cs
@@ -1,11 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebsiteApp.Data;
 
 namespace WebsiteApp.Controllers
 {
     public class ClubController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public ClubController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
+            var clubs = _context.Clubs.ToList();
             return View();
         }
     }
