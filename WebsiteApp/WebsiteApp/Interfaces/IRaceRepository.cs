dcfeed099641c89f3909dcc1447b5e705b5c8b99
Daniel Borisenko <danikborisen19@gmail.com>
2025-02-19 16:17:27 -0500

    refactored whole program for dependency injection + repository pattern

---
+++ WebsiteApp/WebsiteApp/Interfaces/IRaceRepository.cs
@@ -0,0 +1,15 @@
+﻿using WebsiteApp.Models;
+
+namespace WebsiteApp.Interfaces
+{
+    public interface IRaceRepository
+    {
+        Task<IEnumerable<Race>> GetAll();
+        Task<Race> GetByIdAsync(int id);
+        Task<IEnumerable<Race>> GetAllRacesByCity(string city);
+        bool Add(Race race);
+        bool Update(Race race);
+        bool Delete(Race race);
+        bool Save();
+    }
+}
